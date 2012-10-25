cef09f8c2e10625cf86096d88c244fd3952a9138
CodingCat <zhunan@zhunan-2.local>
2012-10-24 22:02:12 -0400

    delete branch

--- install_fair.sh
+++
@@ -1,3 +0,0 @@
-cd src/contrib/fairscheduler/;ant;cd -;
-rm -rf lib/hadoop-fairscheduler-1.0.3.jar;
-cp -v build/contrib/fairscheduler/hadoop-fairscheduler-\$\{version\}.jar lib/hadoop-fairscheduler-1.0.3.jar
